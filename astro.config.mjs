6207a9efad832ee47fbc9da90ff565186d642982
Andreu_67 <andreu.alonsocasasus@gmail.com>
2024-03-12 17:48:27 +0100

    Revert "10/3/24"
    This reverts commit 57310451a7a67611e45b5fb7688df97d7125984b.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,5 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  i18n: {
-    defaultLocale: "es",
-    locales: ["es", "en"],
-    routing: {
-        prefixDefaultLocale: false
-    }
-  },
   integrations: [tailwind(), react()]
 });
