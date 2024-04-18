3d352042854df1b401d86853f08d7ea247c0e895
Andreu_67 <andreu.alonsocasasus@gmail.com>
2024-04-18 22:27:10 +0200

    important changes

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -55,101 +55,85 @@ const NavLinks = ({
 export default function Nav() {
   const [toggled, setToggled] = useState(false);
   return (
-    <nav className="relative mx-8 mb-24 flex items-center justify-between pb-6 pt-12 font-medium md:mx-14 lg:mx-32">
-      <svg
-        className="absolute bottom-0 left-1/2 -translate-x-1/3 md:-translate-x-1/3 lg:-translate-x-1/2"
-        width="250"
-        height="4"
-        viewBox="0 0 250 4"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <motion.path
-          animate={{ pathLength: 1, opacity: 1 }}
-          initial={{ pathLength: 0, opacity: 0 }}
-          transition={{ delay: 1, duration: 0.75 }}
-          transform="translate(-237,0)" 
-          d="M2 2L428 1.99996"
-          stroke="#282828"
-          strokeWidth="2"
-          strokeLinecap="round"
-        />
-      </svg>
+      <nav className="relative mx-8 mb-24 flex items-center justify-between pb-6 pt-12 font-medium md:mx-14 lg:mx-32">
+          <motion.div
+              animate={{opacity: 1, x: 0}}
+              initial={{opacity: 0, x: -25}}
+              transition={{delay: 0.35}}
+              className="flex gap-12"
+          >
+              <a href="/">
+                  <img className="w-20" src={avatar} alt="Logo Mas Camarena"/>
+              </a>
+              <motion.div className="hidden items-center gap-12 xl:flex">
+                  <a href="https://www.facebook.com/ComplejoEducativoMCamarena"><img src={facebook}
+                                                                                     alt="Facebook Account"/></a>
+                  <a href="https://twitter.com/C_Mas_Camarena"><img src={twitter} alt="Twitter Account"/></a>
+                  <a href="https://www.instagram.com/ColegioMasCamarena"><img src={instagram} alt="Instagram Account"/></a>
+              </motion.div>
+          </motion.div>
+          <div className="flex flex-col items-center"> {/* Add this div */}
+              <h1 className="text-lg font-bold">
+                  <a className="relative sm:relative sm:right-50 top-6" href="/">
+                      Exposición PEP
+                  </a>
+              </h1>
+          </div>
 
-      <motion.div
-        animate={{ opacity: 1, x: 0 }}
-        initial={{ opacity: 0, x: -25 }}
-        transition={{ delay: 0.35 }}
-        className="flex gap-12"
-      >
-        <a href="/">
-          <img className="w-20" src={avatar} alt="Logo Mas Camarena" />
-        </a>
-        <motion.div className="hidden items-center gap-12 xl:flex">
-          <a href="https://www.facebook.com/ComplejoEducativoMCamarena"><img src={facebook} alt="Facebook Account" /></a>
-          <a href="https://twitter.com/C_Mas_Camarena"><img src={twitter} alt="Twitter Account" /></a>
-          <a href="https://www.instagram.com/ColegioMasCamarena"><img src={instagram} alt="Instagram Account" /></a>
-        </motion.div>
-      </motion.div>
-      {/* Title */}
+          {/* Title */}
 
-      <h1 className="text-lg font-bold">
-        <a className="relative sm:relative sm:right-50 top-6" href="/">
-          Exposición PEP
-        </a>
-      </h1>
 
-      {/* Nav Items animating in  */}
-      {toggled && (
-        <motion.div
-          variants={navMotion}
-          animate="visible"
-          initial="hidden"
-          className="fixed left-0 top-0  z-40 flex h-screen
+          {/* Nav Items animating in  */}
+          {toggled && (
+              <motion.div
+                  variants={navMotion}
+                  animate="visible"
+                  initial="hidden"
+                  className="fixed left-0 top-0  z-40 flex h-screen
           w-full flex-col items-center  justify-center  gap-24 bg-white text-2xl font-bold"
-        >
-          <NavLinks
-            className="flex flex-col gap-24 text-lg "
-            isMobile={true}
-          />
-        </motion.div>
-      )}
-      <motion.div
-        animate={{ opacity: 1, x: 0 }}
-        initial={{ opacity: 0, x: 25 }}
-        transition={{ delay: 0.35 }}
-        className="hidden xl:flex xl:items-center xl:justify-center xl:gap-12 xl:text-lg   "
-      >
-        <NavLinks className="flex gap-12" isMobile={false} />
-      </motion.div>
+              >
+                  <NavLinks
+                      className="flex flex-col gap-24 text-lg "
+                      isMobile={true}
+                  />
+              </motion.div>
+          )}
+          <motion.div
+              animate={{opacity: 1, x: 0}}
+              initial={{opacity: 0, x: 25}}
+              transition={{delay: 0.35}}
+              className="hidden xl:flex xl:items-center xl:justify-center xl:gap-12 xl:text-lg   "
+          >
+              <NavLinks className="flex gap-12" isMobile={false}/>
+          </motion.div>
 
-      {/* Hamburger Toggle */}
-      <motion.div
-        animate={{ opacity: 1, x: 0 }}
-        initial={{ opacity: 0, x: 25 }}
-        transition={{ delay: 0.35 }}
-        onClick={() => setToggled((prevToggle) => !prevToggle)}
-        className={`burger z-50 cursor-pointer space-y-1.5 xl:hidden 
+          {/* Hamburger Toggle */}
+          <motion.div
+              animate={{opacity: 1, x: 0}}
+              initial={{opacity: 0, x: 25}}
+              transition={{delay: 0.35}}
+              onClick={() => setToggled((prevToggle) => !prevToggle)}
+              className={`burger z-50 cursor-pointer space-y-1.5 xl:hidden 
         `}
-      >
-        <motion.span
-          animate={{ rotateZ: toggled ? 45 : 0, y: toggled ? 8 : 0 }}
-          className="line-1 block h-0.5 w-8 bg-content"
-        ></motion.span>
+          >
+              <motion.span
+                  animate={{rotateZ: toggled ? 45 : 0, y: toggled ? 8 : 0}}
+                  className="line-1 block h-0.5 w-8 bg-content"
+              ></motion.span>
 
-        <motion.span
-          animate={{ width: toggled ? 0 : 24 }}
-          className="line-2 block h-0.5 w-6 bg-content"
-        ></motion.span>
-        <motion.span
-          animate={{
-            rotateZ: toggled ? -45 : 0,
-            y: toggled ? -8 : 0,
-            width: toggled ? 32 : 24,
-          }}
-          className="line-3 block h-0.5 w-4 bg-content"
-        ></motion.span>
-      </motion.div>
-    </nav>
+              <motion.span
+                  animate={{width: toggled ? 0 : 24}}
+                  className="line-2 block h-0.5 w-6 bg-content"
+              ></motion.span>
+              <motion.span
+                  animate={{
+                      rotateZ: toggled ? -45 : 0,
+                      y: toggled ? -8 : 0,
+                      width: toggled ? 32 : 24,
+                  }}
+                  className="line-3 block h-0.5 w-4 bg-content"
+              ></motion.span>
+          </motion.div>
+      </nav>
   );
 }
